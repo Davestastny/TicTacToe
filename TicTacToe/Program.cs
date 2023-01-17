309e46c826365104777ab7cb1c9419f149d94ab2
Daves <Daves@DESKTOP-M2DOM0C>
2023-01-17 23:04:08 +0100

    Přidejte soubory projektu.

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace TicTacToe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string A = "1";
+            string B = "2";
+            string C = "3";
+            string D = "4";
+            string E = "5";
+            string F = "6";
+            string G = "7";
+            string H = "8";
+            string I = "9";
+            string turn = "Player1";
+            string player1, player2;
+
+            Console.WriteLine("Vítejte ve hře");
+            Console.WriteLine("Zmáčkněte jakoukoliv klávesu pro pokračování");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Jméno prvního hráče: ");
+            player1 = Console.ReadLine();
+            Console.WriteLine("Jméno druhého hráče: ");
+            player2 = Console.ReadLine();
+            Console.Clear();
+
+            if (turn == "Player1")
+            {
+
+            }
+        }
+    }
+}
