27172695f52cf20a0d40e474d01c6b3ab6898cb2
David Šťastný <stastny@skolabaltaci.cz>
2023-01-20 17:43:58 +0100

    update 20.1.2023

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -17,7 +17,8 @@ namespace TicTacToe
             string H = "8";
             string I = "9";
             string turn = "Player1";
-            string turn2 = "Player2";
+            string vyhra = "false";
+            string hratZnovu = "false";
             string player1, player2, selection;
 
             Console.WriteLine("Vítejte ve hře");
@@ -28,10 +29,12 @@ namespace TicTacToe
             player1 = Console.ReadLine();
             Console.WriteLine("Jméno druhého hráče: ");
             player2 = Console.ReadLine();
+        Start:
             Console.Clear();
 
             if (turn == "Player1")
             {
+                turn = "Player2";
                 Console.Clear();
                 Console.WriteLine("Player1: " + player1 + "tah");
                 Console.WriteLine("");
@@ -151,37 +154,394 @@ namespace TicTacToe
 
                 if (A == B & B == C)
                 {
+                    vyhra = "true";
+                    Console.WriteLine("Vyhrál " + player1);
+                    Console.WriteLine("");
+                    Console.WriteLine("Chcete hrát znovu? Y / N");
+                    selection = Console.ReadLine();
+                    if (selection == "Y")
+                    {
+                        hratZnovu = "true";
+                    }
+                    else
+                    {
+                        hratZnovu = "false";
+                    }
 
                 }
                 if (D == E & E == F)
                 {
-
+                    vyhra = "true";
+                    Console.WriteLine("Vyhrál " + player1);
+                    Console.WriteLine("");
+                    Console.WriteLine("Chcete hrát znovu? Y / N");
+                    selection = Console.ReadLine();
+                    if (selection == "Y")
+                    {
+                        hratZnovu = "true";
+                    }
+                    else
+                    {
+                        hratZnovu = "false";
+                    }
                 }
                 if (G == H & H == I)
                 {
-
+                    vyhra = "true";
+                    Console.WriteLine("Vyhrál " + player1);
+                    Console.WriteLine("");
+                    Console.WriteLine("Chcete hrát znovu? Y / N");
+                    selection = Console.ReadLine();
+                    if (selection == "Y")
+                    {
+                        hratZnovu = "true";
+                    }
+                    else
+                    {
+                        hratZnovu = "false";
+                    }
                 }
                 if (A == D & D == G)
                 {
-
+                    vyhra = "true";
+                    Console.WriteLine("Vyhrál " + player1);
+                    Console.WriteLine("");
+                    Console.WriteLine("Chcete hrát znovu? Y / N");
+                    selection = Console.ReadLine();
+                    if (selection == "Y")
+                    {
+                        hratZnovu = "true";
+                    }
+                    else
+                    {
+                        hratZnovu = "false";
+                    }
                 }
                 if (B == E & E == H)
                 {
-
+                    vyhra = "true";
+                    Console.WriteLine("Vyhrál " + player1);
+                    Console.WriteLine("");
+                    Console.WriteLine("Chcete hrát znovu? Y / N");
+                    selection = Console.ReadLine();
+                    if (selection == "Y")
+                    {
+                        hratZnovu = "true";
+                    }
+                    else
+                    {
+                        hratZnovu = "false";
+                    }
                 }
                 if (C == F & F == I)
                 {
-
+                    vyhra = "true";
+                    Console.WriteLine("Vyhrál " + player1);
+                    Console.WriteLine("");
+                    Console.WriteLine("Chcete hrát znovu? Y / N");
+                    selection = Console.ReadLine();
+                    if (selection == "Y")
+                    {
+                        hratZnovu = "true";
+                    }
+                    else
+                    {
+                        hratZnovu = "false";
+                    }
                 }
                 if (G == E & E == C)
                 {
-
+                    vyhra = "true";
+                    Console.WriteLine("Vyhrál " + player1);
+                    Console.WriteLine("");
+                    Console.WriteLine("Chcete hrát znovu? Y / N");
+                    selection = Console.ReadLine();
+                    if (selection == "Y")
+                    {
+                        hratZnovu = "true";
+                    }
+                    else
+                    {
+                        hratZnovu = "false";
+                    }
                 }
                 if (A == E & E == I)
                 {
-
+                    vyhra = "true";
+                    Console.WriteLine("Vyhrál " + player1);
+                    Console.WriteLine("");
+                    Console.WriteLine("Chcete hrát znovu? Y / N");
+                    selection = Console.ReadLine();
+                    if (selection == "Y")
+                    {
+                        hratZnovu = "true";
+                    }
+                    else
+                    {
+                        hratZnovu = "false";
+                    }
                 }
             }
+
+            if (turn == "Player2")
+            {
+                turn = "Player1";
+                Console.Clear();
+                Console.WriteLine("Player2: " + player2 + "tah");
+                Console.WriteLine("");
+                Console.WriteLine(A + "|" + B + "|" + C);
+                Console.WriteLine(D + "|" + E + "|" + F);
+                Console.WriteLine(G + "|" + H + "|" + I);
+                Console.WriteLine("****");
+                Console.WriteLine("Rozhodněte se");
+                selection = Console.ReadLine();
+                if (selection == "1")
+                {
+                    if (A == "1")
+                    {
+                        A = "O";
+                    }
+                    else
+                    {
+                        Console.WriteLine("Políčko je používané");
+                    }
+                }
+
+                if (selection == "2")
+                {
+                    if (B == "2")
+                    {
+                        B = "O";
+                    }
+                    else
+                    {
+                        Console.WriteLine("Políčko je používané");
+                    }
+                }
+
+                if (selection == "3")
+                {
+                    if (C == "3")
+                    {
+                        C = "O";
+                    }
+                    else
+                    {
+                        Console.WriteLine("Políčko je používané");
+                    }
+                }
+
+                if (selection == "4")
+                {
+                    if (D == "4")
+                    {
+                        D = "O";
+                    }
+                    else
+                    {
+                        Console.WriteLine("Políčko je používané");
+                    }
+                }
+
+                if (selection == "5")
+                {
+                    if (E == "5")
+                    {
+                        E = "O";
+                    }
+                    else
+                    {
+                        Console.WriteLine("Políčko je používané");
+                    }
+                }
+
+                if (selection == "6")
+                {
+                    if (F == "6")
+                    {
+                        F = "O";
+                    }
+                    else
+                    {
+                        Console.WriteLine("Políčko je používané");
+                    }
+                }
+
+                if (selection == "7")
+                {
+                    if (G == "7")
+                    {
+                        G = "O";
+                    }
+                    else
+                    {
+                        Console.WriteLine("Políčko je používané");
+                    }
+                }
+
+                if (selection == "8")
+                {
+                    if (H == "8")
+                    {
+                        H = "O";
+                    }
+                    else
+                    {
+                        Console.WriteLine("Políčko je používané");
+                    }
+                }
+
+                if (selection == "9")
+                {
+                    if (I == "9")
+                    {
+                        I = "O";
+                    }
+                    else
+                    {
+                        Console.WriteLine("Políčko je používané");
+                    }
+                }
+
+                if (A == B & B == C)
+                {
+                    vyhra = "true";
+                    Console.WriteLine("Vyhrál " + player2);
+                    Console.WriteLine("");
+                    Console.WriteLine("Chcete hrát znovu? Y / N");
+                    selection = Console.ReadLine();
+                    if (selection == "Y")
+                    {
+                        hratZnovu = "true";
+                    }
+                    else
+                    {
+                        hratZnovu = "false";
+                    }
+
+                }
+                if (D == E & E == F)
+                {
+                    vyhra = "true";
+                    Console.WriteLine("Vyhrál " + player2);
+                    Console.WriteLine("");
+                    Console.WriteLine("Chcete hrát znovu? Y / N");
+                    selection = Console.ReadLine();
+                    if (selection == "Y")
+                    {
+                        hratZnovu = "true";
+                    }
+                    else
+                    {
+                        hratZnovu = "false";
+                    }
+                }
+                if (G == H & H == I)
+                {
+                    vyhra = "true";
+                    Console.WriteLine("Vyhrál " + player2);
+                    Console.WriteLine("");
+                    Console.WriteLine("Chcete hrát znovu? Y / N");
+                    selection = Console.ReadLine();
+                    if (selection == "Y")
+                    {
+                        hratZnovu = "true";
+                    }
+                    else
+                    {
+                        hratZnovu = "false";
+                    }
+                }
+                if (A == D & D == G)
+                {
+                    vyhra = "true";
+                    Console.WriteLine("Vyhrál " + player1);
+                    Console.WriteLine("");
+                    Console.WriteLine("Chcete hrát znovu? Y / N");
+                    selection = Console.ReadLine();
+                    if (selection == "Y")
+                    {
+                        hratZnovu = "true";
+                    }
+                    else
+                    {
+                        hratZnovu = "false";
+                    }
+                }
+                if (B == E & E == H)
+                {
+                    vyhra = "true";
+                    Console.WriteLine("Vyhrál " + player2);
+                    Console.WriteLine("");
+                    Console.WriteLine("Chcete hrát znovu? Y / N");
+                    selection = Console.ReadLine();
+                    if (selection == "Y")
+                    {
+                        hratZnovu = "true";
+                    }
+                    else
+                    {
+                        hratZnovu = "false";
+                    }
+                }
+                if (C == F & F == I)
+                {
+                    vyhra = "true";
+                    Console.WriteLine("Vyhrál " + player2);
+                    Console.WriteLine("");
+                    Console.WriteLine("Chcete hrát znovu? Y / N");
+                    selection = Console.ReadLine();
+                    if (selection == "Y")
+                    {
+                        hratZnovu = "true";
+                    }
+                    else
+                    {
+                        hratZnovu = "false";
+                    }
+                }
+                if (G == E & E == C)
+                {
+                    vyhra = "true";
+                    Console.WriteLine("Vyhrál " + player2);
+                    Console.WriteLine("");
+                    Console.WriteLine("Chcete hrát znovu? Y / N");
+                    selection = Console.ReadLine();
+                    if (selection == "Y")
+                    {
+                        hratZnovu = "true";
+                    }
+                    else
+                    {
+                        hratZnovu = "false";
+                    }
+                }
+                if (A == E & E == I)
+                {
+                    vyhra = "true";
+                    Console.WriteLine("Vyhrál " + player2);
+                    Console.WriteLine("");
+                    Console.WriteLine("Chcete hrát znovu? Y / N");
+                    selection = Console.ReadLine();
+                    if (selection == "Y")
+                    {
+                        hratZnovu = "true";
+                    }
+                    else
+                    {
+                        hratZnovu = "false";
+                    }
+                }
+            }
+            if (vyhra == "false")
+            {
+                goto Start;
+            }
+            if (hratZnovu == "true")
+            {
+                goto Start;
+            }
+
         }
     }
 }
